2fbd6a4ae7927f2d108b65a1bc1c3329fc5fe236
minwang03 <93901911+minwang03@users.noreply.github.com>
2025-05-26 07:40:01 +0700

    u

--- app/src/main/java/com/example/facesach/ui/LoginActivity.java
+++ app/src/main/java/com/example/facesach/ui/LoginActivity.java
@@ -1,6 +1,12 @@
 package com.example.facesach.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.facesach.R;
@@ -10,5 +16,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText inputEmail = findViewById(R.id.input_email);
+        EditText inputPassword = findViewById(R.id.input_password);
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                if (email.equals("quang123") && password.equals("q")) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else Toast.makeText(LoginActivity.this, "Sai tài khoản hoặc mật khẩu", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
